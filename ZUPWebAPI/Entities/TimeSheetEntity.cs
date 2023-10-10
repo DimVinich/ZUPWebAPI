83d4e3abee7226d5fb54edec39e4181c0d56d7dd
Dmitrij Sokolov <DmitrijVS@gmail.com>
2023-10-10 17:12:35 +0300

    Чего по табелю делал

---
+++ ZUPWebAPI/Entities/TimeSheetEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace ZUPWebAPI.Entities
+{
+    public class TimeSheetEntity
+    {
+        public int idEmployee { get; set; }
+        public DateTime Date { get; set; } 
+        public string TypeDate { get; set; }
+        public int Worked { get; set; }
+    }
+}
---
+++ ZUPWebAPI/Models/TimeSheetGetData.cs
@@ -0,0 +1,8 @@
+﻿namespace ZUPWebAPI.Models
+{
+    public class TimeSheetGetData : UserAuthenticationData
+    {
+        public int idEmploee { get; set; }
+        public int idPost { get; set; }
+    }
+}
