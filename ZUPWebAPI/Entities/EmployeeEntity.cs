05a7dee89dfa2224deef989b5b6b52dc4ef6f02b
DimVinich <DmitrijVS@gmail.com>
2023-11-08 13:13:20 +0300

    тест со значениями нулл

--- ZUPWebAPI/Entities/EmployeeEntity.cs
+++ ZUPWebAPI/Entities/EmployeeEntity.cs
@@ -3,17 +3,17 @@
     public class EmployeeEntity
     {
         public int idEmployee { get; set; }
-        public string nEmployeeShort { get; set; }
-        public string nEmployee { get; set;}
-        public string ActualAddress { get; set; }
-        public string RegisteredAddress { get; set;}
-        public string eMail { get; set; }
-        public string TIN { get; set; }
+        public string? nEmployeeShort { get; set; }
+        public string? nEmployee { get; set;}
+        public string? ActualAddress { get; set; }
+        public string? RegisteredAddress { get; set;}
+        public string? eMail { get; set; }
+        public string? TIN { get; set; }
         public int idPost { get; set; }
         public int idUnit { get; set; }
-        public string PassportNumbers { get; set; }
-        public string PassportIssued { get; set; }    
-        public DateTime PassportDate { get; set; }
-        public DateTime DateOfEmloyment { get; set; }
+        public string? PassportNumbers { get; set; }
+        public string? PassportIssued { get; set; }    
+        public DateTime? PassportDate { get; set; } = DateTime.Now;
+        public DateTime? DateOfEmloyment { get; set; } = DateTime.Now;
     }
 }
